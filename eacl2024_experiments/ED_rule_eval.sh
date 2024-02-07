41b99040ba235db8c7e4b7163b51a1bbadd6bbe9
shigashiyama <mapion.benri@gmail.com>
2024-02-07 23:33:34 +0900

    Add scripts for rule-ED

---
+++ eacl2024_experiments/ED_rule_eval.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Need to set appropriate paths
+
+DB_PATH=../data/osm/20230620_all_extnames.txt
+FILE_GOLD=../../atd-mcl/atd-mcl/full/main/split-118/json/test-all.json
+FILE_NAME=`basename $FILE_GOLD`
+FILE_PRED=../eacl2024_experiments/results/ED/rule/$FILE_NAME
+
+cd eval_scripts
+
+poetry run python eval_scripts/ed_evaluator_for_groups.py \
+       -d $DB_PATH \
+       -g $FILE_GOLD \
+       -p $FILE_PRED
---
+++ eacl2024_experiments/ED_rule_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Need to set appropriate paths
+
+DB_PATH=../data/osm/20230620_all_extnames.txt
+FILE_GOLD=../../atd-mcl/atd-mcl/full/main/split-118/json/test-all.json
+FILE_NAME=`basename $FILE_GOLD`
+DIR_PRED=../eacl2024_experiments/results/ED/rule
+FILE_PRED=$DIR_PRED/$FILE_NAME
+
+cd rule_based
+
+mkdir -p $DIR_PRED
+
+poetry run python rule_based/entity_disambiguator.py \
+       -d $DB_PATH \
+       -i $FILE_GOLD \
+       -o $FILE_PRED
